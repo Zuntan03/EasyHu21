957ce99d4d74637325d08191ffe6912d641f4ede
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-09-15 04:28:36 +0900

    起動batのオプション追加。

--- ComfyUi.bat
+++ ComfyUi.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001 > NUL
 
-call %~dp0ComfyUi_NoArgs.bat --auto-launch --max-upload-size 500 
+call %~dp0ComfyUi_NoArgs.bat --auto-launch --max-upload-size 500 --fast --use-sage-attention %*
 @REM --fast --use-sage-attention %*
